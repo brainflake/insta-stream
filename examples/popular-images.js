ca5c2249d09f4fa3c39af80f0b9a737b92ef8fc1
falk <falk@logicparty.org>
2013-01-04 16:09:57 -0500

    take out client_id/secret

--- examples/popular-images.js
+++ examples/popular-images.js
@@ -1,7 +1,7 @@
 var insta = require('..')
   , _ = require('underscore')
 
-var Instagram = new insta({client_id: 'b6911936292d4ddda1767aba4b38e9a7', client_secret: '4429264c9d3a40189b383da9868a2b80'})
+var Instagram = new insta({client_id: 'CLIENT_ID', client_secret: 'CLIENT_SECRET'})
 
 Instagram.stream('popular', '', function(stream) {
   stream.on('data', function(data) {
